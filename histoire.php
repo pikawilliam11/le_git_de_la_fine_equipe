3c2a5808e1f9819857c63a7e73bb3d9c5bcee867
Emile76 <emiledesilets76@gmail.com>
2020-10-27 16:45:45 -0400

    Animations_page_histoire_ED
    Elles sont miennes.

---
+++ histoire.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link rel="stylesheet" href="https://use.typekit.net/rcw0dji.css">
+    <link rel="stylesheet" href="stylesheet_histoire.css">
+    <meta charset="UTF-8">
+        <title>NQ_page_histoire_total_ED_V2</title>
+    </head>
+    <body>   
+        <div class="ouverture">
+            <div class="wrapper">
+              <div class="fleurTotale">
+                <img class="fleur fno4" src="https://i.imgur.com/zWrF4C8.png" alt="fleur4">
+        
+                <img class="fleur fno3" src="https://i.imgur.com/Xty2Q5c.png" alt="fleur3">
+        
+                <img class="fleur fno2" src="https://i.imgur.com/UGlHWRr.png" alt="fleur2">
+        
+                <img class="fleur fno1" src="https://i.imgur.com/51mg0Xz.png" alt="fleur1">
+             </div>
+           </div>
+            
+            <div class="texteOuverture">
+              
+              <h2>Quatre décennies au coeur <br> de l'enjeu environnemental</h2>
+              <div class="boiteOuverture">
+                <p>En 1981, Harvey Mead et un petit groupe d’impliqués dans des organismes locaux créent le Front commun québécois pour les espaces verts (qui deviendra l’UQCN, puis Nature Québec). Il est formé en réaction à plusieurs projets qui menacent les milieux naturels aux battures de Beauport, au Kamouraska et autour du lac Saint-Pierre.</p>
+              </div>  
+                <div class="boiteOuverture">
+                <p>Au fil des années, Nature Québec acquiert une notoriété publique et devient la première ONG québécoise d’envergure dédiée à la conservation des milieux naturels et de la biodiversité. </p>
+              </div> 
+                
+              <div class="boiteOuverture">
+                <p>L’organisme publiera notamment le premier magazine québécois consacré à la nature et l’environnement Franc-Nord / Franc-vert et se positionnera publiquement dans plusieurs dossiers qui contribueront à bâtir sa crédibilité.</p>
+              </div> 
+        
+                <div class="boiteOuverture">
+                <p>Aujourd’hui, Nature Québec compte plus de 15 employés permanents et collabore avec des dizaines d’experts pour la publication de nombreux rapports et mémoires. Nature Québec donne aussi plusieurs activités de sensibilisation/conférences et participe à la réalisation des nombreux projets sur le terrain en ville comme en région.</p>
+              </div> 
+         </div>
+         </div>
+        
+        <div class="section1980">
+          
+          <div class="decennie1980">
+            
+            <div class="imagesTotal">
+              
+                <img class="image1980 no1" src="https://i.imgur.com/BACP2eC.png" alt="ciel">
+           
+                <img class="image1980 pasCiel no1.1" src="https://i.imgur.com/fgrbiNV.png" alt="eau_fond">
+        
+                <img class="image1980 pasCiel no2" src="https://imgur.com/OPG26MT.png" alt="eau_montagne_background">
+        
+                <img class="image1980 pasCiel no3" src="https://imgur.com/ABBiwMp.png" alt="montagne_background">
+        
+                <img class="image1980 pasCiel no4" src="https://i.imgur.com/pH6O0qX.png" alt="eau_montagne_left">
+        
+                <img class="image1980 pasCiel no5" src="https://i.imgur.com/71tgbRB.png" alt="montagne_left">
+        
+                <img class="image1980 pasCiel no6" src="https://i.imgur.com/d8CIkiN.png" alt="eau_montagne_front">
+              
+                <img class="image1980 pasCiel no7 " src="https://i.imgur.com/3wHDiXx.png" alt="montagne_front">
+              
+                 <img class="image1980 pasCiel no8" src="https://i.imgur.com/GFTR9HH.png" alt="eau_top"> 
+              
+             </div>
+              
+               <div class="texte1980">
+        
+                  <div class="boite1980 no1">
+                    <h2>1981</h2>
+                    <p>Fondation du Front commun québécois pour les espaces verts et les sites naturels.</p>
+                  </div>
+        
+                  <div class="boite1980 no2">
+                    <h2>1982</h2>
+                    <p>Participation aux audiences du BAPE sur la foresterie, lesquelles aboutissent à l’adoption de la Loi sur les forêts.</p>
+                  </div>
+        
+                  <div class="boite1980 no3">
+                    <h2>1983</h2>
+                    <p>Le Front commun québécois pour les espaces verts et les sites naturels devient l’Union québécoise pour la conservation de la nature (UQCN).</p>
+                  </div>
+        
+                  <div class="boite1980 no4">
+                    <h2>1984</h2>
+                    <p>Parution du premier numéro de Franc-Nord, magazine qui informe sur le patrimoine naturel et humain des paysages québécois.</p>
+                    </div>
+                  <div class="boite1980 no5">
+                    <h2>1986</h2>
+                    <p>Adoption de la Loi sur les forêts qui découle du travail de concertation de l’UQCN et d’autres organismes.</p>
+                  </div>
+        
+                  <div class="boite1980 no6">
+                    <h2>1987</h2>
+                    <p>L’UQCN dévoile les premiers gagnants du concours de photos La nature du Québec en images du magazine Franc-Nord.</p>
+                  </div>
+        
+                  <div class="boite1980 no7">
+                    <h2>1988</h2>
+                    <p>Sous la direction de l’UQCN, douze organismes riverains fondent Stratégies Saint-Laurent pour veiller à la gestion intégrée du fleuve.</p>
+                    </div>
+        
+                  <div class="boite1980 no8">
+                    <h2>1989</h2>
+                    <p>Harvey Mead, président-fondateur de l’UQCN, devient sous-ministre adjoint au développement durable et à la conservation au ministère de l’Environnement, à la suite de revendications de plusieurs organismes environnementaux.</p>
+                  </div>
+              </div>
+          </div>
+        </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.3/ScrollTrigger.min.js"></script>
+    <script src="script_histoire.js"></script>
+</body> 
+</html>
---
+++ script_histoire.js
@@ -0,0 +1,132 @@
+
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.timeline({
+            scrollTrigger:{
+            pin: true,
+            scrub: 1,
+            start: 'center center',
+            end: '300% center',
+            toggleActions: 'play complete reverse reset',
+            trigger: ".ouverture",
+            },
+        })
+        .to(".texteOuverture", {
+            opacity:0,
+            y:"-25%",
+            stagger:0.2,
+        ease: "circ",
+        })
+
+        .to(".fleurTotale", {
+            rotation: "360deg",
+            scale:1,
+            y:"-50%",
+            ease: "power2",
+        }, '<')
+
+        .to(".fno1", {
+            rotation: "360deg",
+            scale:1.5,
+            ease: "none",
+        }, '>')
+            
+        .to(".fno2", {
+        rotation: "-360deg",
+            scale:2,
+        ease: "none",
+        }, '<')
+            
+        .to(".fno3", {
+            rotation: "360deg",
+            scale:2.5,
+        ease: "none",
+        }, '<')
+            
+        .to(".fno4", {
+            rotation: "-360deg",
+            scale:3,
+        ease: "none",
+        }, '<')
+
+        gsap.timeline({
+            scrollTrigger:{
+            scrub:1,
+            start: 'top bottom',
+            end: 'center center',
+            toggleActions: 'play complete reverse reverse',
+            trigger: ".section1980",
+            },
+        })
+
+        .to(".fleurTotale", {
+            y:"-140%",
+            ease: "none",
+        })
+
+        .fromTo(".fno1", {
+            rotation: "0deg",
+        }, {
+            rotation: "360deg",
+            ease: "none",
+        }, '<')
+
+        .fromTo(".fno2", {
+            rotation: "0deg",
+        },{
+            rotation: "-360deg",
+            ease: "none",
+        }, '<')
+
+        .fromTo(".fno3", {
+            rotation: "0deg",
+        }, {
+            rotation: "360deg",
+            ease: "none",
+        }, '<')
+
+        .fromTo(".fno4", {
+            rotation: "0deg",
+        }, {
+            rotation: "360deg",
+            ease: "none",
+        }, '<')
+
+
+
+        gsap.timeline({
+            scrollTrigger:{
+            pin: true,
+            scrub: 1,
+            start: 'center center',
+            end: 'bottom center',
+            toggleActions: 'play complete reverse reset',
+            trigger: ".section1980",
+            },
+        })
+        .from(".no3", {
+            y:"100%",
+            duration:1.5,
+        })
+        .from(".no5", {
+            y:"100%",
+        duration:1.5,
+        }, '<0.5')
+        .from(".no7", {
+            y:"100%",
+        duration:1.5,
+        }, '<0.5')
+
+        gsap.timeline({
+            scrollTrigger:{
+            pin: true,
+            scrub: 1,
+            start: '50.1% center',
+            end: '200% center',
+            toggleActions: 'play complete reverse reset',
+            trigger: ".section1980",
+            },
+        })
+        .to(".texte1980", {  
+            y:"-122%",
+        }, '>') 
