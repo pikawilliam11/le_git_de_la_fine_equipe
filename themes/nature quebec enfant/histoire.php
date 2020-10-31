0e6893a2665ed7bcdce286dd81a6c0242af1b78f
pikawilliam11 <cashwilliam@hotmail.com>
2020-10-31 14:28:47 -0400

    Theme

---
+++ themes/nature quebec enfant/histoire.php
@@ -0,0 +1,348 @@
+<?php 
+  /*Template Name: Histoire*/
+?>
+
+<?php
+
+// Appel le fichier header.php
+get_header(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link rel="stylesheet" href="https://use.typekit.net/rcw0dji.css">
+    <!--<link rel="stylesheet" href="stylesheet_histoire.css">-->
+    <link rel="stylesheet" hreft="./style.css">
+    <meta charset="UTF-8">
+        <title>NQ_page_histoire_total_ED_V2</title>
+    </head>
+    <body>   
+      <!--Partie d'Émile-->
+        <div class="ouverture">
+            <div class="wrapper">
+              <div class="fleurTotale">
+                <img class="fleur fno4" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur4.png" alt="fleur4">
+        
+                <img class="fleur fno3" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur3.png" alt="fleur3">
+        
+                <img class="fleur fno2" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur2.png" alt="fleur2">
+        
+                <img class="fleur fno1" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur1.png" alt="fleur1">
+             </div>
+           </div>
+            
+            <div class="texteOuverture">
+              
+              <h2>Quatre décennies au coeur <br> de l'enjeu environnemental</h2>
+              <div class="boiteOuverture">
+                <p>En 1981, Harvey Mead et un petit groupe d’impliqués dans des organismes locaux créent le Front commun québécois pour les espaces verts (qui deviendra l’UQCN, puis Nature Québec). Il est formé en réaction à plusieurs projets qui menacent les milieux naturels aux battures de Beauport, au Kamouraska et autour du lac Saint-Pierre.</p>
+              </div>  
+                <div class="boiteOuverture">
+                <p>Au fil des années, Nature Québec acquiert une notoriété publique et devient la première ONG québécoise d’envergure dédiée à la conservation des milieux naturels et de la biodiversité. </p>
+              </div> 
+                
+              <div class="boiteOuverture">
+                <p>L’organisme publiera notamment le premier magazine québécois consacré à la nature et l’environnement Franc-Nord / Franc-vert et se positionnera publiquement dans plusieurs dossiers qui contribueront à bâtir sa crédibilité.</p>
+              </div> 
+        
+                <div class="boiteOuverture">
+                <p>Aujourd’hui, Nature Québec compte plus de 15 employés permanents et collabore avec des dizaines d’experts pour la publication de nombreux rapports et mémoires. Nature Québec donne aussi plusieurs activités de sensibilisation/conférences et participe à la réalisation des nombreux projets sur le terrain en ville comme en région.</p>
+              </div> 
+         </div>
+         </div>
+        
+        <div class="section1980">
+          
+          <div class="decennie1980">
+            
+            <div class="imagesTotal">
+              
+                <img class="image1980 no1" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/ciel.png" alt="ciel">
+           
+                <img class="image1980 pasCiel no1.1" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/eau_fond.png" alt="eau_fond">
+        
+                <img class="image1980 pasCiel no2" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/eau_montagne_background.png" alt="eau_montagne_background">
+        
+                <img class="image1980 pasCiel no3" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/montagne_background.png" alt="montagne_background">
+        
+                <img class="image1980 pasCiel no4" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/eau_montagne_left.png" alt="eau_montagne_left">
+        
+                <img class="image1980 pasCiel no5" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/eau_montagne_left.png" alt="montagne_left">
+        
+                <img class="image1980 pasCiel no6" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/eau_montagne_front.png" alt="eau_montagne_front">
+              
+                <img class="image1980 pasCiel no7 " src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/montagne_avant_plan4.png" alt="montagne_front">
+              
+                 <img class="image1980 pasCiel no8" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/Eau_top.png" alt="eau_top"> 
+              
+             </div>
+              
+               <div class="texte1980">
+        
+                  <div class="boite1980 no1">
+                    <h2>1981</h2>
+                    <p>Fondation du Front commun québécois pour les espaces verts et les sites naturels.</p>
+                  </div>
+        
+                  <div class="boite1980 no2">
+                    <h2>1982</h2>
+                    <p>Participation aux audiences du BAPE sur la foresterie, lesquelles aboutissent à l’adoption de la Loi sur les forêts.</p>
+                  </div>
+        
+                  <div class="boite1980 no3">
+                    <h2>1983</h2>
+                    <p>Le Front commun québécois pour les espaces verts et les sites naturels devient l’Union québécoise pour la conservation de la nature (UQCN).</p>
+                  </div>
+        
+                  <div class="boite1980 no4">
+                    <h2>1984</h2>
+                    <p>Parution du premier numéro de Franc-Nord, magazine qui informe sur le patrimoine naturel et humain des paysages québécois.</p>
+                    </div>
+                  <div class="boite1980 no5">
+                    <h2>1986</h2>
+                    <p>Adoption de la Loi sur les forêts qui découle du travail de concertation de l’UQCN et d’autres organismes.</p>
+                  </div>
+        
+                  <div class="boite1980 no6">
+                    <h2>1987</h2>
+                    <p>L’UQCN dévoile les premiers gagnants du concours de photos La nature du Québec en images du magazine Franc-Nord.</p>
+                  </div>
+        
+                  <div class="boite1980 no7">
+                    <h2>1988</h2>
+                    <p>Sous la direction de l’UQCN, douze organismes riverains fondent Stratégies Saint-Laurent pour veiller à la gestion intégrée du fleuve.</p>
+                    </div>
+        
+                  <div class="boite1980 no8">
+                    <h2>1989</h2>
+                    <p>Harvey Mead, président-fondateur de l’UQCN, devient sous-ministre adjoint au développement durable et à la conservation au ministère de l’Environnement, à la suite de revendications de plusieurs organismes environnementaux.</p>
+                  </div>
+              </div>
+          </div>
+        </div>
+<!--Partie de William-->
+        <div class="wrapper1990">
+        <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/gsap-1-ciel.jpg" alt="ciel" class="ciel-1-wd">
+        <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/gsap-1-terre.png" alt="terre" class="terre-1-wd">
+
+        <div class="texte1990">
+            <div class="box1990">
+                <h2>1990</h2>
+                <p>Franc-Nord devient Franc-Vert pour indiquer plus clairement son engagement écologique.</p>
+              </div>
+              <div class="box1990">
+                <h2>1992</h2>
+                <p>Participation au développement de la Politique québécoise sur les espèces menacées ou vulnérables.</p>
+              </div>
+              <div class="box1990">
+                <h2>1993</h2>
+                <p>Production par l’UQCN du Guide des milieux humides du Québec : des sites à découvrir et à protéger.</p>
+              </div>
+              <div class="box1990">
+                <h2>1994</h2>
+                <p>Succès de la coalition formée par l’Association des biologistes du Québec (ABQ), la WWF et l’UQCN pour empêcher la privatisation de la moitié des parcs provinciaux.</p>
+              </div>
+              <div class="box1990">
+                <h2>1995</h2>
+                <p>Lancement de l’Écoroute, dont l’objectif est de diffuser de l’information sur l’environnement et de répondre aux internautes.
+    
+                    À la suite des pressions exercées par l’UQCN et d’autres organismes, le Règlement sur l’analyse et l’évaluation environnementale est appliqué aux grands projets industriels.</p>
+              </div>
+              <div class="box1990">
+                <h2>1996</h2>
+                <p>Adoption de la politique énergétique à la suite des consultations de l’ensemble des acteurs, dont l’UQCN.</p>
+              </div>
+              <div class="box1990">
+                <h2>1997</h2>
+                <p>Formation de la Coalition pour la forêt vierge nordique afin de promouvoir une meilleure gestion de la forêt boréale et la création d’aires protégées.</p>
+              </div>
+              <div class="box1990">
+                <h2>1999</h2>
+                <p>Fin de la publication du magazine Franc-Vert qui renaîtra en format web.</p>
+              </div>
+        </div>
+
+        <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/gsap-1-eau.png" alt="eau" class="eau-1-wd">
+    </div>
+    
+    <div class="wrapper2000">
+        <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/gsap-2-ciel.jpg" alt="ciel" class="ciel-2-wd">
+        <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/gsap-2-eau.png" alt="terre" class="eau-2-wd">
+        <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/gsap-2-montagne-1.png" alt="montagne-part-1" class="montagne-part-1-wd">
+        <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/gsap-2-montagne-2.png" alt="montagne-part-2" class="montagne-part-2-wd">
+
+        <div class="texte2000">
+            <div class="box2000">
+                <h2>2000</h2>
+                <p>Mise en place de la Stratégie québécoise sur les aires protégées au terme d’une campagne sur les espaces en danger menée par le World Wildlife Fund (WWF), avec le soutien de l’UQCN.</p>
+              </div>
+              <div class="box2000">
+                <h2>2001</h2>
+                <p>L’UQCN fonde l’initiative Aux arbres citoyens! et devient mandataire au Québec du programme Zones importantes pour la conservation des oiseaux (ZICO).</p>
+              </div>
+              <div class="box2000">
+                <h2>2002</h2>
+                <p>
+                  Après des années d’interventions de l’UQCN, moratoire sur la production porcine et dépôt de la Politique nationale de l’eau par le gouvernement du Québec.</p>
+              </div>
+              <div class="box2000">
+                <h2>2003</h2>
+                <p>Formation de la commission Coulombe sur l’état de la forêt, en réponse aux pressions exercées par l’UQCN et d’autres organismes.</p>
+              </div>
+              <div class="box2000">
+                <h2>2004</h2>
+                <p>L’UQCN est le seul groupe environnemental présent au Comité national de rétablissement du caribou forestier. Relance des Ateliers sur la conservation des milieux naturels.</p>
+              </div>
+              <div class="box2000">
+                <h2>2005</h2>
+                <p>L’Union québécoise pour la conservation de la nature (UQCN) devient Nature Québec.</p>
+              </div>
+              <div class="box2000">
+                <h2>2006</h2>
+                <p>Nature Québec a 25 ans. Fondation de la coalition SOS Parc Orford, laquelle vise à protéger l’intégrité du parc. Dépôt à l’Assemblée nationale la pétition On dort comme une bûche!, qui recueille un nombre record de 185 000 signatures.</p>
+              </div>
+              <div class="box2000">
+                <h2>2007</h2>
+                <p>Siège au comité directeur du Sommet sur la forêt à Québec et participation à la Commission sur l’avenir de l’agriculture et de l’agroalimentaire québécois. Nature Québec condamne la vente de terrains adjacents au parc national des Îles-de-Boucherville à des fins de développement résidentiel.</p>
+              </div>
+              <div class="box2000">
+                <h2>2008</h2>
+                <p>
+                  Publication d’une étude sur l’identification d’aires protégées permettant le maintien du caribou forestier.
+                  
+                  Lancement d’une campagne sur les risques d’un nouveau mode de gestion des débits d’eau à la sortie des Grands-Lacs.</p>
+              </div>
+              <div class="box2000">
+                <h2>2009</h2>
+                <p>
+                  Réalisation d’une étude importante étude sur le suivi de la biodiversité dans les aires protégées et consolidation du programme ZICO.</p>
+              </div>
+        </div>
+    </div>
+
+    <!--Partie de Jeremy-->
+    <div class="section2010">
+        
+        <div class="decennie2010">
+          
+          <div class="imagesTotal">
+            
+              <img class="image2010 no1" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/derriere.png"alt="derriere">
+         
+              <img class="image2010 flipable no2" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/millieu.png" alt="millieu">
+      
+              <img class="image2010 flipable no3" src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/devant.png" alt="devant">
+      
+            </div>
+            
+             <div class="texte2010">
+      
+                <div class="boite2010 no1">
+                  <h2>2010</h2>
+                  <p>Contribue à la définition de la Loi sur l’aménagement durable du territoire forestier et réalisation du projet Agriculture et climat : vers des fermes 0 carbone.</p>
+                </div>
+      
+                <div class="boite2010 no2">
+                  <h2>2012</h2>
+                  <p>Implication majeure au sein du mouvement Sortons le Québec du nucléaire et obtention de la fermeture de la centrale nucléaire Gentilly-2.</p>
+                </div>
+      
+                <div class="boite2010 no3">
+                  <h2>2013</h2>
+                  <p>
+      Lancement du projet Carcaj’où? pour sensibiliser le public et les communautés nordiques autochtones et non autochtones à la précarité de l’espèce.</p>
+                </div>
+      
+                <div class="boite2010 no4">
+                  <h2>2014</h2>
+                  <p>Campagne et pétition de 100 000 noms pour s’opposer à la construction d’un terminal pétrolier lié au projet Énergie Est au coeur de la pouponnière des bélugas du Saint-Laurent à Cacouna. Injonction obtenue par Me Michel Bélanger au nom de Nature Québec et 3 autres groupes pour stopper temporairement les travaux dans l’habitat du Béluga.
+      
+      Début de la campagne Nagoya + pour l’atteinte des objectifs de création des aires protégées.</p>
+                  </div>
+                <div class="boite2010 no5">
+                  <h2>2015</h2>
+                  <p>
+      Lancement du programme MVS (Milieux de vie en santé) qui démontre les bienfaits des îlots de fraîcheur sur la santé par des projets de verdissement des espaces urbains.
+      
+      Abandon par Transcanada de son terminal pétrolier à Cacouna suite à une mobilisation sans précédent. La Cour fédérale donne raison à Nature Québec et au CQDE en forçant le fédéral à protéger l’habitat de la Rainette faux-grillon de l’Ouest. Lancement du programme Milieux de vie en santé. Coordination de la Marche Action Climat qui réunit plus de 25 000 personnes à Québec.</p>
+                </div>
+      
+                <div class="boite2010 no6">
+                  <h2>2016</h2>
+                  <p>
+      Lancement de la campagne #FuturAnticosti contre les projets d’hydrocarbures sur l’île et organisation de la 1ère Grande Traversée, une marche inédite de 130 km sur Anticosti.</p>
+                </div>
+      
+                <div class="boite2010 no7">
+                  <h2>2017</h2>
+                  <p>Grande victoire avec l’abandon définitif du projet d’oléoduc Énergie Est.
+      
+      Nature Québec, l’Action Boréale et la SNAP obtiennent l’arrêt du projet de déplacer les derniers caribous de Val-d’Or au zoo de Saint-Félicien.
+      
+      Lancement de la coalition SOS mont Hereford contre un projet de ligne aérienne d’Hydro-Québec dans une aire protégée et de la coalition SOS port de Québec, contre le projet Beauport 2020. Lancement de la campagne NON aux forages contre les projets d’hydrocarbures sur le territoire québécois.</p>
+                  </div>
+      
+                <div class="boite2010 no8">
+                  <h2>2018</h2>
+                  <p>Retrait officiel des projets gaziers et pétroliers sur Anticosti. Aval de Québec et d’Ottawa pour soutenir la candidature de l’île au patrimoine mondial de l’UNESCO. Hydro-Québec accepte d’enfouir la ligne qui traversera le Mont-Hereford mais le projet est suspendu. Nature Québec, l’Action Boréale et Greenpeace lancent une campagne pour la sauvegarde des derniers caribous de Val-d’Or.
+      
+      Réalisation de ruelles vertes, de classes en plein air et d’oasis urbaines dans la région de Québec et extension de Milieux de vie en santé à Trois-Rivières, Victoriaville et Sherbrooke.</p>
+                </div> 
+        </div>
+          
+       </div>
+    
+    <div class="placeHolder"></div>
+<div class="wrapper2010">
+  <div class="fleurPrincipale">
+    <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur4.png" alt="fleur4" class="bigGirl">
+  </div>
+
+  <div class="fleurSecondaire">
+    <div class="fs1">
+       <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur4.png" alt="fleur4">>
+    </div>
+    <div class="fs2">
+              <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur4.png" alt="fleur4">
+    </div>
+    <div class="fs3">
+      <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur4.png" alt="fleur4">
+    </div>
+    <div class="fs4">
+       <img src="http://127.0.0.1/naturequebec/wp-content/uploads/2020/10/fleur4.png" alt="fleur4">
+    </div>
+  </div>
+</div>
+<div class="fermeture">
+  <div class="texteFermeture">
+      
+      <h2>On ne s'arrête pas là!</h2>
+      <div class="boiteFermeture">
+        <p>Nous avons peut-être accomplie de grande chose jusqu'à présent grace à votre soutient, mais ils nous restent encore beaucoup de combat à mener et de cause à défendre afin de garder notre planète en santé.</p>
+      </div>  
+        <div class="boiteFermeture">
+        <p>En tant que première ONG québécoise d’envergure dédiée à la conservation des milieux naturels et de la biodiversité, nous faisons de notre  devoir de s'impliquer dans une multitude de projet environnementaux chaques années, afin de montrer l'explample et de trouver des solution à long terme pour restorer et protèger notre belle planète bleu. </p>
+      </div> 
+        
+      <div class="boiteFermeture">
+        <p>Tout ceci serait impossible sans le support généreux de nos partenaire et des gens comme vous qui donnent un peu de leur argent dur gagner pour la cause environnemental.</p>
+      </div> 
+
+        <div class="boiteFermeture">
+        <p>Un gros merci de toutes l'équipe de Nature Quebec à tous ceux qui on ou vont nous donnez leur soutient.</p>
+      </div> 
+ </div>
+ </div>
+
+ </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js"></script>
+    <script src="script_histoire.js"></script>
+</body> 
+
+<?php
+ //Appel le fichier footer.php
+get_footer(); ?>
+
+</html>
