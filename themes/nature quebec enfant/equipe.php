cff36c8ae42f78ffd06b75813fd4c9e58e2fa8ac
pikawilliam11 <cashwilliam@hotmail.com>
2020-11-24 11:11:05 -0500

    Toutes les pages PHP demandées

---
+++ themes/nature quebec enfant/equipe.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Modèle permettant d'afficher une page.
+ */
+
+// Appel le fichier header.php
+get_header(); 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Equipe</title>
+</head>
+<body>
+	
+</body>
+</html>
+
+<?php else : // Si aucune page correspondante n'a été trouvée ?>
+	<h2>Oh oh, la page recherchée est disparue</h2>
+	<img src="https://i.giphy.com/media/3o8doR2qGIXQDGCVoY/giphy.webp" alt="Page disparue">
+<?php endif; 
+
+// Appel le fichier footer.php
+get_footer(); ?>
