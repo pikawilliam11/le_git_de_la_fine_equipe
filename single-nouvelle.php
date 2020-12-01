41966f019f29e56279489623cbef2398a819f38a
Herojay13 <54539405+Herojay13@users.noreply.github.com>
2020-12-01 14:32:16 -0500

    les nouvelles sont fini

--- single-nouvelle.php
+++ single-nouvelle.php
@@ -22,19 +22,31 @@
 </head>
 <body>
   
-      <div class="partie1">
-        <div class="titre"> <?php the_field('titre'); ?> </div>
-        <div class="datePublication"> <?php the_field('date_de_publication'); ?> </div>
-      </div>
+        <div class="partie1">
+          <div class="titre"> <?php the_field('titre'); ?> </div>
+          <div class="datePublication"> <?php the_field('date_de_publication'); ?> </div>
+        </div>
   <div class="essaie">
-      <div class="texte"> <?php the_field('texte'); ?> </div>
-
-      <div class="images">
+        
+    <div class="partie2">
       <img src="<?php the_field('image1'); ?>" alt="" class="Image un">
-      <img src="<?php the_field('image2'); ?>" alt="" class="Image deux">
-      <img src="<?php the_field('image3'); ?>" alt="" class="Image trois">
-      </div>
+      <div class="texte block1"> <?php the_field('texte1'); ?> </div>
+    </div>
+    
+    <div class="partie3">
+      <img src="<?php the_field('image2'); ?>" alt="" class="Image deux"> 
+      <div class="texte block2"> <?php the_field('texte2'); ?> </div>
+    </div> 
+    
+    <div class="partie4">
+      <img src="<?php the_field('image3'); ?>" alt="" class="Image trois"> 
+      <div class="texte block3"> <?php the_field('texte3'); ?> </div>
+    </div>
+        
+
   </div> 
+     
+    <div class="auteurs">
       <div class="auteur1">
         <img src="<?php the_field('image_auteur'); ?>" alt="" class="ImageAut1">
         <div class="nomAuteur1"> <?php the_field('nom_auteur'); ?> </div>
@@ -44,7 +56,7 @@
         <img src="<?php the_field('image_auteur_2'); ?>" alt="" class="ImageAut2">
         <div class="nomAuteur2"> <?php the_field('nom_auteur_2'); ?> </div> 
       </div>
-               
+    </div>           
 </body>
 
 <?php
